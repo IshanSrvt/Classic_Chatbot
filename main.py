0f85621690d9066369dee6e98fbd0677a889145a
IshanSrvt <76547283+IshanSrvt@users.noreply.github.com>
2022-11-26 14:01:34 +0530

    Main Code Commit

---
+++ main.py
@@ -0,0 +1,89 @@
+import time
+
+y = []
+def sort():
+    #x = words.index(i)
+
+   # print(x)
+    if i == 'and':
+        y.append(i)
+        print( "removing word 'and' ") 
+
+    elif i == 'please':
+        y.append(i)
+        print( "removing word 'please' ")
+
+    elif i == 'tell':
+        y.append(i)
+        print( "removing word 'tell' ")
+
+    elif i == 'can':
+        y.append(i)
+        print( "removing word 'can' ")
+
+    elif i == 'you':
+        y.append(i)
+        print( "removing word 'you' ")
+
+    elif i == 'is':
+        y.append(i)
+        print( "removing word 'is' ")
+
+    elif i == 'a':
+        y.append(i)
+        print( "removing word 'a' ")
+        
+    elif i == 'an':
+        y.append(i)
+        print( "removing word 'an' ")
+    else:
+        print(f"no worries in word '{i}'")
+    print("ignored words list: ", y)
+
+
+def format_list():
+    print("")
+    print("Sorting..")
+    for i in y:
+        try:
+            words.remove(i)
+        except ValueError:
+            pass
+
+
+
+
+user_inp = input("Input: ")
+#print(f"input = {user_inp}")
+user_low = user_inp.lower()
+print(f"lower_case: {user_low}")
+
+user_split = user_low.split()
+print(f"split: {user_split}")
+
+words = user_split
+print(words)
+
+for i in user_split:
+   # print(i)
+    print(" ")
+    print("Validating")
+    time.sleep(2)
+    sort()
+    #print(words)
+
+format_list()
+time.sleep(3)
+#print("split = ",user_split)
+
+if words[0] or words[1] == "what" or "what's" or "whats":
+    time.sleep(3)
+    print("")
+    print("FOUND THE WORD 'what'/'what's'/'whats' ")
+    time.sleep(2)
+    #print(words[0]," ",words[1])
+    print("Bag Of Words = ",words)
+else:
+    time.sleep(2)
+    print("End of loop")
+
